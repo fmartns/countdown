a0f8bab0f34e915daacbff4ec6c2c3b4bef18463
Filipe <fmartns66@gmail.com>
2024-08-12 12:09:17 -0300

    Coutdown

---
+++ assets/js/scripts.js
@@ -0,0 +1,31 @@
+function updateCountdown() {
+    const targetDate = new Date("2024-09-04T17:00:00-03:00");
+    const now = new Date();
+    
+    // Calcule a diferença de tempo
+    const timeDifference = targetDate - now;
+  
+    if (timeDifference <= 0) {
+        document.getElementById("days").innerText = "00";
+        document.getElementById("hours").innerText = "00";
+        document.getElementById("minutes").innerText = "00";
+        document.getElementById("seconds").innerText = "00";
+        return;
+    }
+  
+    const days = Math.floor(
+        (timeDifference % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+    );
+    const hours = Math.floor(
+        (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  
+    document.getElementById("days").innerText = String(days).padStart(2, "0");
+    document.getElementById("hours").innerText = String(hours).padStart(2, "0");
+    document.getElementById("minutes").innerText = String(minutes).padStart(2, "0");
+    document.getElementById("seconds").innerText = String(seconds).padStart(2, "0");
+}
+  
+setInterval(updateCountdown, 1000);
