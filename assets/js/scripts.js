de3545e8f897e5d42ef57f4cc82860d2bd95055d
Filipe Martins <fmartns66@gmail.com>
2024-08-12 19:24:08 -0300

    add months

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,9 +10,13 @@ function updateCountdown() {
         document.getElementById("hours").innerText = "00";
         document.getElementById("minutes").innerText = "00";
         document.getElementById("seconds").innerText = "00";
+        document.getElementById("months").innerText = "00";
         return;
     }
   
+    const months= Math.floor(
+        (timeDifference % (1000 * 60 * 60 * 24 * 30 * 12)) / (1000 * 60 * 60 * 24 *30)
+    );
     const days = Math.floor(
         (timeDifference % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
     );
@@ -22,6 +26,7 @@ function updateCountdown() {
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
   
+    document.getElementById("months").innerText = String(months).padStart(2, "0");
     document.getElementById("days").innerText = String(days).padStart(2, "0");
     document.getElementById("hours").innerText = String(hours).padStart(2, "0");
     document.getElementById("minutes").innerText = String(minutes).padStart(2, "0");
